105d7feb7d72544f13699fcd78a63780bfa64308
PyryL <pyry.laaksonen@tuni.fi>
2025-01-28 12:13:28 +0200

    Boilerplate for stm32 uart

--- stm32/User/Inc/own_main.h
+++ stm32/User/Inc/own_main.h
@@ -1,1 +1,4 @@
-void own_main();
+#include "stm32f4xx.h"
+
+void own_main(UART_HandleTypeDef huart);
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart);
--- stm32/User/Src/own_main.c
+++ stm32/User/Src/own_main.c
@@ -1,8 +1,46 @@
 #include "own_main.h"
-#include "stm32f407xx.h"
 #include "main.h"
+#include <string.h>
 
-void own_main(){
+uint8_t recv_buf[128];
+uint8_t final_buf[128];
+uint8_t temp;
+int idx = 0;
+uint8_t received = 0;
 
+void own_main(UART_HandleTypeDef huart){
+
+    HAL_UART_Receive_IT(&huart, &temp, 1);
+    while(1){
+    	if(received == 1){
+    		//do something
+    		received = 0;
+    		HAL_UART_Receive_IT(&huart, &temp, 1);
+    	}
+    }
+
+}
+
+/**
+ * @brief UART receive complete callback
+ *
+ * UART data is received one byte at a time, with a maximum of 128 bytes.
+ * When data is received, it is copied into recv_buf. The temp buf is checked for
+ * '}' which means that the message has ended.
+ */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart){
+	if(temp == '}'){
+		memcpy(final_buf, recv_buf, idx);
+		received = 1;
+		idx = 0;
+	}else{
+		memcpy(recv_buf + idx, &temp, 1);
+		if(idx >= 128){
+			idx = 0;
+		} else {
+			++idx;
+		}
+		HAL_UART_Receive_IT(huart, &temp, 1);
+	}
 }
 
